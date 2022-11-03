fd0c9d387bf8f09296ee7fe5fde9ed7d66a392bd
Divine-20 <divineingabire69@gmail.com>
2022-11-03 08:03:56 +0200

    worked on navbar

--- components/forms/signin.tsx
+++ components/forms/signin.tsx
@@ -9,7 +9,7 @@ export default function Signin(){
 
             </div>
             {/* <div className='relative'> */}
-            <div className="flex mt-8 ">
+            <div className="flex mt-8 justify-center">
             <div className='mt-6'>
              <Image 
               width={600}
@@ -26,9 +26,11 @@ export default function Signin(){
              <form action="" className='block mt-16  ml-[25%]'>
                 <input type="email" placeholder='Email' className=' block border-solid border border-[#6C63FF] border-opacity-10 h-14  w-5/6 rounded-lg pl-8 '/>
                 <input type="password" placeholder='Password' className='block border-solid border border-[#6C63FF] border-opacity-10 h-14  w-5/6  rounded-lg pl-8 mt-4'/>
-                <div className="flex justify-evenly">
-                <input type="checkbox" className='h-4 w-4 mt-4 border-solid border border-[#6C63FF] border-opacity-10 flex' /><span className='pl-2 font-karia pt-3'>Remember me</span>
-                <a href="" className="pt-3 font-karia text-[#6C63FF] ">Forgot password</a>
+                <div className="flex justify-evenly mt-4">
+                <div className="flex ">
+                <input type="checkbox" className='h-4 w-4 mt-4 relative  border-solid border border-[#6C63FF] border-opacity-10 flex' /><span className='pl-2 font-karia pt-3 '>Remember me</span>
+                 </div>
+                <a href="" className="pt-3 font-karia text-[#6C63FF]">Forgot password?</a>
                 </div>
                 <button className='rounded-lg bg-[#6C63FF] block text-white w-52 h-12 ml-auto mr-auto mt-6 font-semibold'>Log in</button>
                 <div className='flex mt-4 ml-16'>
@@ -37,7 +39,7 @@ export default function Signin(){
                 <img src="/icons/line1.svg" alt="line1" />
                 </div>
                 <button className='bg-[#D9D9D9] bg-opacity-10 flex shadow-md font-karia  shadow-slate-400  rounded-lg h-12 w-64 text-center items-center pl-8 ml-auto mr-auto mt-4'><img src="/icons/google.svg" alt="google" className='pr-3'/>Sign In With Google</button>
-                <p className='font-karia pt-8 text-center'>Don't have an account?<Link href = "/auth/login" className='text-[#6C63FF] pl-1'>Sign up</Link></p>
+                <p className='font-karia pt-8 text-center'>Don't have an account?<Link href = "/dashboard/navbar" className='text-[#6C63FF] pl-1'>Sign up</Link></p>
              </form> 
           
           </div>
--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Signin from '../../components/forms/signin'
 
+
 const login = () => {
   return (
     <div>
         <Signin/>
+        
     </div>
   )
 }
