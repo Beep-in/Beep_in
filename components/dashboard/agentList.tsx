4e01d515ec397ea8f4325d016dc0a9331a070190
Divine-20 <divineingabire69@gmail.com>
2022-12-19 18:52:45 +0200

    re-styled agents list

--- components/dashboard/agentList.tsx
+++ components/dashboard/agentList.tsx
@@ -1,144 +1,138 @@
-import React, {useState} from 'react'
-import { Upload } from "react-bootstrap-icons";
-import { Check2All, Trash3 } from "react-bootstrap-icons";
+import React, { useState } from "react";
+import { Check2All, Trash3,CircleFill} from "react-bootstrap-icons";
 
 import { AiOutlineEye } from "react-icons/ai";
 import { BiChevronRightCircle, BiChevronLeftCircle } from "react-icons/bi";
 function AgentList() {
-    const tableData = [
-        {
-          date: "Agent 123KZ",
-          messageStatus: "0788733422",
-          recipient: "agent123@gmail.com",
-          mobileNumber: "Agent 123KZ",
-        },
-        {
-          date: "2020-10-12",
-          messageStatus: "Sent",
-          recipient: "KALIGIRWA Sonia",
-          mobileNumber: "+2507783466743",
-        },
-        {
-          date: "2020-10-12",
-          messageStatus: "Sent",
-          recipient: "KALIGIRWA Sonia",
-          mobileNumber: "+2507783466743",
-        },
-        {
-          date: "2020-10-12",
-          messageStatus: "Sent",
-          recipient: "KALIGIRWA Sonia",
-          mobileNumber: "+2507783466743",
-        },
-        {
-          date: "2020-10-12",
-          messageStatus: "Sent",
-          recipient: "KALIGIRWA Sonia",
-          mobileNumber: "+2507783466743",
-        },
-        {
-          date: "2020-10-12",
-          messageStatus: "Sent",
-          recipient: "KALIGIRWA Sonia",
-          mobileNumber: "+2507783466743",
-        },
-        {
-          date: "2020-10-12",
-          messageStatus: "Sent",
-          recipient: "KALIGIRWA Sonia",
-          mobileNumber: "+2507783466743",
-        },
-        {
-          date: "2020-10-12",
-          messageStatus: "Sent",
-          recipient: "KALIGIRWA Sonia",
-          mobileNumber: "+2507783466743",
-        },
-      ];
+  const tableData = [
+    {
+      name: "Agent 123KZ",
+      mobileNumber: "0788733422",
+      email: "agent123@gmail.com",
+      category: "Agent 123KZ",
+      accountStatus: "Active",
+    },
+    {
+      name: "Agent 123KZ",
+      mobileNumber: "0788733422",
+      email: "agent123@gmail.com",
+      category: "Agent 123KZ",
+      accountStatus: "Active",
+    },
+    {
+      name: "Agent 123KZ",
+      mobileNumber: "0788733422",
+      email: "agent123@gmail.com",
+      category: "Agent 123KZ",
+      accountStatus: "Active",
+    },
+    {
+      name: "Agent 123KZ",
+      mobileNumber: "0788733422",
+      email: "agent123@gmail.com",
+      category: "Agent 123KZ",
+      accountStatus: "Active",
+    },
+    {
+      name: "Agent 123KZ",
+      mobileNumber: "0788733422",
+      email: "agent123@gmail.com",
+      category: "Agent 123KZ",
+      accountStatus: "Active",
+    },
+    {
+      name: "Agent 123KZ",
+      mobileNumber: "0788733422",
+      email: "agent123@gmail.com",
+      category: "Agent 123KZ",
+      accountStatus: "Active",
+    },
+    {
+      name: "Agent 123KZ",
+      mobileNumber: "0788733422",
+      email: "agent123@gmail.com",
+      category: "Agent 123KZ",
+      accountStatus: "Active",
+    },
+
+  ];
   return (
-    <div className='w-full'>
-    <div className="h-20 w-full border-b-2 border-solid flex justify-center float-right items-center">
+    <div className="w-full">
+      <div className="h-20 w-full border-b-2 border-solid flex justify-center float-right items-center">
         <h1>Agents List</h1>
       </div>
       <div className="h-96 w-4/5 mt-20 absolute right-4">
-          <div className="flex flex-col mt-4">
-            <div className="overflow-x-auto">
-              <div className="p-1.5 w-full inline-block align-middle">
-                <div className="overflow-hidden ">
-                  <table className="min-w-full divide-y divide-gray-200">
-                    <thead className="bg-gray-50">
+        <div className="flex flex-col mt-4">
+          <div className="overflow-x-auto">
+            <div className="p-1.5 w-full inline-block ">
+              <div className="overflow-hidden ">
+                <table className="min-w-full divide-y divide-gray-200">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th scope="col" className="px-6 py-3  text-left ">
+                        Agent name
+                      </th>
+                      <th
+                        // scope="col"
+                        className=" py-3 placeholder:text-left  "
+                      >
+                        Mobile Number
+                      </th>
+                      <th scope="col" className="px-24 py-3  text-left  ">
+                        Email
+                      </th>
+                      <th scope="col" className="px-6 py-3 text-right  ">
+                        Category
+                      </th>
+                      <th scope="col" className="px-24 py-3  text-left">
+                        Account Status
+                      </th>
+                      <th scope="col" className=" py-3  text-left ">
+                        Actions
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="divide-y divide-gray-200">
+                    {tableData.map((sms_data) => (
                       <tr>
-                        <th
-                          scope="col"
-                          className="px-6 py-3  text-left "
-                        >
-                          Agent name
-                        </th>
-                        <th
-                          scope="col"
-                          className="px-6 py-3 placeholder:text-left  "
-                        >
-                          Mobile Number
-                        </th>
-                        <th
-                          scope="col"
-                          className="px-6 py-3  text-left  "
-                        >
-                          Email
-                        </th>
-                        <th
-                          scope="col"
-                          className="px-6 py-3 ttext-right  "
-                        >
-                          Category
-                        </th>
-                        <th
-                          scope="col"
-                          className="px-6 py-3  text-right"
-                        >
-                          Account Status
-                        </th>
-                        <th
-                          scope="col"
-                          className="px-6 py-3  text-right  "
-                        >
-                          Actions
-                        </th>
+                        <td className="px-6 py-5 text-sm font-medium text-gray-800 whitespace-nowrap">
+                          {sms_data.name}
+                        </td>
+                        <td className="px-12 py-5 text-sm text-gray-800 whitespace-nowrap flex">
+                          {sms_data.mobileNumber}
+                        </td>
+                        <td className="px-24 py-5 text-sm text-gray-800 whitespace-nowrap">
+                          {sms_data.email}
+                        </td>
+                        <td className="py-5 text-sm font-medium text-right whitespace-nowrap">
+                          {sms_data.category}
+                        </td>
+                        <td className="px-24 py-5 text-sm font-medium text-right whitespace-nowrap flex">
+                          {sms_data.accountStatus}
+                          <CircleFill className="text-green-500 ml-4 text-xs"/>
+                        </td>
+                        <td className="py-3">
+                         <button className="text-blue-500 bg-[#6C63FF] bg-opacity-20 h-8 w-20 rounded-lg mr-6">Update</button>
+                         <label className="inline-flex relative items-center mr-5 cursor-pointer">
+                        <input type="checkbox" value="" className="sr-only peer"/>
+                        <div className="w-11 h-6 bg-green-500 rounded-full peer peer-focus:ring-4 peer-focus:ring-white dark:peer-focus:ring-green-800 dark:bg-green-500 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-red-600"></div>
+                         </label>
+                        </td>
                       </tr>
-                    </thead>
-                    <tbody className="divide-y divide-gray-200">
-                      {tableData.map((sms_data) => (
-                        <tr>
-                          <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
-                            {sms_data.date}
-                          </td>
-                          <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap flex">
-                            {sms_data.messageStatus}</td>
-                          <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                            {sms_data.recipient}
-                          </td>
-                          <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                            {sms_data.mobileNumber}
-                          </td>
-                          <td className="px-6 py-4 text-sm font-medium text-right absolute right-4  gap-2  whitespace-nowrap flex">
-                            <AiOutlineEye className="text-blue-500 text-xl" />
-                            <Trash3 className="text-red-500 text-xl" />
-                          </td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
+                    ))}
+                  </tbody>
+                </table>
               </div>
             </div>
           </div>
-          <div className="flex justify-center mt-2">
-            <BiChevronLeftCircle className="text-3xl text-blue-500" />
-            <BiChevronRightCircle className="text-3xl text-blue-500" />
-          </div>
+        </div>
+        <div className="flex justify-center mt-2">
+          <BiChevronLeftCircle className="text-3xl text-blue-500" />
+          <BiChevronRightCircle className="text-3xl text-blue-500" />
         </div>
       </div>
-  )
+    </div>
+  );
 }
 
-export default AgentList
+export default AgentList;
